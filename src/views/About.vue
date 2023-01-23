3d697f44d446b62fbfeff72bc844fabad3cf8606
AbiodunVlad <omonijoabiodun@gmail.com>
2023-01-23 18:05:58 +0100

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <h3>Hi {{ name }}!</h3>
+    <h4>{{ student.name }}</h4>
+    <h4>{{ student.school }}</h4>
+    <p>Here is the about page</p>
+    <button @click="changeName">Change Name</button>
+    <!-- <button @click="changeStudent">Change Student</button> -->
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from "@vue/reactivity";
+const name = ref("Vlad");
+
+const student = reactive({
+  name: "Vlad",
+  school: "AltSchool",
+});
+
+const changeName = () => {
+  name.value = "Abiodun Vlad";
+};
+
+// const changeStudent = () => {
+//   student.value = { name: "Titi", school: "OAU" };
+// };
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Here is the home page</p>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
